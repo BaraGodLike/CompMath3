f2e0f0ee6e3147a1db47ab9228ea4665fe34fb41
BaraGodLike <drago.vlad@bk.ru>
2025-03-23 18:56:22 +0300

    Update: processing of break points

--- Integrator.py
+++ Integrator.py
@@ -1,3 +1,5 @@
+import math
+
 from methods import rectangleleft, rectangleright, rectanglemid, trapezoid, simpson
 
 
@@ -16,7 +18,7 @@ class Integrator:
         1: {
             "name": "Метод прямоугольников (правый)",
             "func": rectangleright.RectangleRight.execute,
-            },
+        },
         2: {
             "name": "Метод прямоугольников (средний)",
             "func": rectanglemid.RectangleMid.execute,
@@ -31,6 +33,12 @@ class Integrator:
         }
     }
 
+    def try_func(self, x):
+        try:
+            return self.func(x)
+        except (ZeroDivisionError, OverflowError, ValueError):
+            return None
+
     def runge_rule(self, method, n=4):
         I_n = method(n, self.func, self.a, self.b)
         I_2n = method(2 * n, self.func, self.a, self.b)
@@ -40,16 +48,89 @@ class Integrator:
             I_2n = method(2 * n, self.func, self.a, self.b)
         return I_2n, n
 
+    def find_breakpoints(self, n):
+        breakpoints = set()
+
+        if self.try_func(self.a) is None:
+            breakpoints.add(self.a)
+
+        if self.try_func(self.b) is None:
+            breakpoints.add(self.b)
+
+        if self.try_func((self.a + self.b) / 2) is None:
+            breakpoints.add((self.a + self.b) / 2)
+
+        h = (self.b - self.a) / n
+        for i in range(n):
+            point = self.a + i * h
+            if self.try_func(self.a + i * h) is None:
+                breakpoints.add(point)
+
+        return list(breakpoints)
+
+    def is_convergent(self, breakpoints):
+        for bp in breakpoints:
+            y1 = self.try_func(bp - self.eps)
+            y2 = self.try_func(bp + self.eps)
+            if ((y1 is not None)
+                    and (y2 is not None) and ((abs(y1 - y2) > self.eps) or (y1 == y2 and y1 is not None))):
+                return False
+        return True
+
     def integrate(self, method_number):
         """
-                'rectangle_left': 0 \n
-                'rectangle_right': 1 \n
-                'rectangle_mid': 2 \n
-                'trapezoid': 3 \n
-                'simpson': 4
-            """
+            'rectangle_left': 0 \n
+            'rectangle_right': 1 \n
+            'rectangle_mid': 2 \n
+            'trapezoid': 3 \n
+            'simpson': 4
+        """
+        breakpoints = self.find_breakpoints(int((self.b - self.a) * 1000))
         method = self.methods[method_number]["func"]
-        if method:
+
+        if len(breakpoints) == 0:
             return self.runge_rule(method)
+
+        if not self.is_convergent(breakpoints):
+            raise Exception("Интеграл не существует")
+
+        # ОДНА ТОЧКА РАЗРЫВА
+        if len(breakpoints) == 1:
+            if self.a in breakpoints:
+                self.a += self.eps
+            elif self.b in breakpoints:
+                self.b -= self.eps
+
+        # МНОЖЕСТВО ТОЧЕК РАЗРЫВА
         else:
-            raise Exception("Неизвестный метод")
+            res = 0
+            n = 0
+
+            # РАЗРЫВ В ПЕРВОМ ПОДИНТЕРВАЛЕ
+            if not (self.try_func(self.a) is None or self.try_func(breakpoints[0] - self.eps) is None):
+                integrator = Integrator(self.func, self.a, breakpoints[0] - self.eps, self.eps)
+                results = integrator.integrate(method_number)
+                res += results[0]
+                n += results[1]
+
+            # РАЗРЫВ В ПОСЛЕДНЕМ ПОДИНТЕРВАЛЕ
+            if not (self.try_func(self.b) is None or self.try_func(breakpoints[0] + self.eps) is None):
+                integrator = Integrator(self.func, breakpoints[0] + self.eps, self.b, self.eps)
+                results = integrator.integrate(method_number)
+                res += results[0]
+                n += results[1]
+
+            # ОСТАЛЬНЫЕ РАЗРЫВЫ
+            for break_index in range(len(breakpoints) - 1):
+                break_cur = breakpoints[break_index]
+                break_next = breakpoints[break_index + 1]
+
+                if not (self.try_func(break_cur + self.eps) is None or self.try_func(break_next - self.eps) is None):
+                    integrator = Integrator(self.func, break_cur + self.eps, break_next - self.eps, self.eps)
+                    results = integrator.integrate(method_number)
+                    res += results[0]
+                    n += results[1]
+            return res, n
+
+        if not breakpoints or self.a - self.eps in breakpoints or self.b + self.eps in breakpoints:
+            return self.runge_rule(method)
--- functions.py
+++ functions.py
@@ -1,11 +1,59 @@
+import math
+
+
 def func1(x):
     """x ^ 3 - 5 * x ^ 2 + 3 * x - 16"""
-    return x**3 - 5*x**2 + 3*x - 16
+    return x ** 3 - 5 * x ** 2 + 3 * x - 16
+
+
+def func2(x):
+    """-x ^ 3 - x ^ 2 - 2 * x + 1"""
+    return -x ** 3 - x ** 2 - 2 * x + 1
+
+
+def func3(x):
+    """-3 * x ^ 3 - 5 * x ^ 2 + 4 * x - 2"""
+    return -3 * x ** 3 - 5 * x ** 2 + 4 * x - 2
+
+
+def func4(x):
+    """1 / (1 - x)"""
+    return 1 / (1 - x)
+
+
+def func5(x):
+    """1 / sqrt(x)"""
+    return 1 / math.sqrt(x)
+
+
+def func6(x):
+    """1 / sqrt(1 - x)"""
+    return 1/math.sqrt(1 - x)
 
 
 functions = {
     0: {
         "name": "x ^ 3 - 5 * x ^ 2 + 3 * x - 16",
         "func": func1
-    }
+    },
+    1: {
+        "name": "-x ^ 3 - x ^ 2 - 2 * x + 1",
+        "func": func2
+    },
+    2: {
+        "name": "-3 * x ^ 3 - 5 * x ^ 2 + 4 * x - 2",
+        "func": func3
+    },
+    3: {
+        "name": "1 / (1 - x)",
+        "func": func4
+    },
+    4: {
+        "name": "1 / sqrt(x)",
+        "func": func5
+    },
+    5: {
+        "name": "1 / sqrt(1 - x)",
+        "func": func6
+    },
 }
